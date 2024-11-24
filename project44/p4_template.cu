0041bdf31729237db1a84227834e045f37b94762
Steve Charles <steve.charles50123456@gmail.com>
2024-11-24 14:36:39 -0500

    test

--- project44/p4_template.cu
+++ project44/p4_template.cu
@@ -101,7 +101,7 @@ void faxpyCuda(int N, float alpha, float* xarray, float* yarray, float* resultar
     const int threadsPerBlock = 512;
     const int blocks = (N + threadsPerBlock - 1) / threadsPerBlock;
 
-    float* x, y, result;
+    void* x, y, result;
     float* d_x, d_y, d_result;
 
     //
